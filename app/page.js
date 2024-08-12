7a9093fff2c00b6bc7c769d0e27fbb8c896b5909
Shakil-Ahmmed-Wakil <95299126+Shakil-Ahmmed-Wakil@users.noreply.github.com>
2024-08-12 14:05:36 +0600

    update app/page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import {
   FaBell,
   FaBookmark,
+  FaFaceSmile,
   FaHouse,
+  FaLocationDot,
   FaMagnifyingGlass,
   FaRightFromBracket,
+  FaUser,
   FaUsers,
 } from "react-icons/fa6";
 
@@ -15,64 +18,89 @@ export default function Home() {
         <div className="search-bar bg-gray-300 px-5 py-2.5 rounded-full flex items-center gap-2.5">
           <FaMagnifyingGlass />
           <input
-            className="bg-transparent"
-            type="search"
-            name="search"
-            id="search"
-            placeholder="Search for creators, inspirations, and projects"
+          className="bg-transparent"
+          type="search"
+          name="search"
+          id="search"
+          placeholder="Search for creators, inspirations, and projects"
           />
-        </div>
-        <div className="profile-section flex gap-5">
-          <button
-            className="create bg-blue-500 px-8 text-white rounded-full"
-            type="button"
+      </div>
+      <div className="profile-section flex gap-5">
+        <button
+          className="create bg-blue-500 px-8 text-white rounded-full"
+          type="button"
           >
-            Create
-          </button>
-          <img
-            className="profile-picture w-12 rounded-full"
-            src="profile.jpg"
-            alt="Profile Picture"
-          />
-        </div>
-      </header>
+          Create
+        </button>
+        <img
+        className="profile-picture w-12 rounded-full"
+        src="profile.jpg"
+        alt="Profile Picture"
+        />
+    </div>
+  </header>
 
-      <section className="px-8">
-        <aside className="bg-white rounded-md shadow-md w-fit">
-          <ul>
-            <li>
-              <button className="aside-nav-item">
-                <FaHouse />
-                <span>Home</span>
-              </button>
-            </li>
-            <li>
-              <button className="aside-nav-item">
-                <FaUsers />
-                <span>Friends</span>
-              </button>
-            </li>
-            <li>
-              <button className="aside-nav-item">
-                <FaBookmark />
-                <span>Saved Posts</span>
-              </button>
-            </li>
-            <li>
-              <button className="aside-nav-item">
-                <FaBell />
-                <span>Notifications</span>
-              </button>
-            </li>
-            <li>
-              <button className="aside-nav-item">
-                <FaRightFromBracket />
-                <span>Logout</span>
-              </button>
-            </li>
-          </ul>
-        </aside>
+  <section className="px-8">
+    <aside className="bg-white rounded-md shadow-md w-fit">
+      <ul>
+        <li>
+          <button className="aside-nav-item">
+            <FaHouse />
+            <span>Home</span>
+          </button>
+        </li>
+        <li>
+          <button className="aside-nav-item">
+            <FaUsers />
+            <span>Friends</span>
+          </button>
+        </li>
+        <li>
+          <button className="aside-nav-item">
+            <FaBookmark />
+            <span>Saved Posts</span>
+          </button>
+        </li>
+        <li>
+          <button className="aside-nav-item">
+            <FaBell />
+            <span>Notifications</span>
+          </button>
+        </li>
+        <li>
+          <button className="aside-nav-item">
+            <FaRightFromBracket />
+            <span>Logout</span>
+          </button>
+        </li>
+      </ul>
+    </aside>
+    <main className="flex flex-column gap-5">
+      <section className="create-post">
+        <div>
+        <img className="profile-picture w-12 rounded-full" src="profile.jpg" alt="Profile Picture" />
+        <textarea placeholder="What's on your mind?"></textarea>
+        </div>
+        <div>
+          <div className="flex items-center gap-2.5">
+            <button className="icon-text-btn">
+          <FaUser />
+          <span>People</span>
+            </button>
+            <button className="icon-text-btn">
+          <FaLocationDot />
+          <span>Check In</span>
+            </button>
+            <button className="icon-text-btn">
+          <FaFaceSmile />
+          <span>Mood</span>
+            </button>
+          </div>
+          <button className="rounded-md bg-blue-500 text-white">Share</button>
+        </div>
       </section>
-    </>
+    </main>
+  </section>
+</section>
   );
-}
+  }
